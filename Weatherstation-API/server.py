c189f1474d143ba6e918fc761e6fca057b89ad43
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2024-05-26 20:20:19 -0700

    Implemented endpoint for adding sensor data

--- Weatherstation-API/server.py
+++ Weatherstation-API/server.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 from Model.sensor_reading_entity import SensorReading
 from sqlobject import connectionForURI, sqlhub
 import constants
@@ -15,3 +15,16 @@ create_tables()
 # Create the Flask instance, this will be used to create the api endpoints
 app = Flask(__name__)
 
+
+@app.post('/add_sensor_reading')
+def add_sensor_reading_route():
+    """ Endpoint to add a sensor reading to the database """
+    try:
+        add_sensor_reading(request.form.to_dict())
+        return 201
+    except (ValueError, TypeError) as e:
+        # If this function raises either a ValueError or a TypeError, then
+        # the data supplied in the request is invalid. So send back an error to the user
+        return jsonify({
+            'error': str(e)
+        }), 400  # Return the error message and a 400 status code
