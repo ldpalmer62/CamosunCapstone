9161b88e731387d98764f8fb06ed049f501f5e75
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2024-05-26 19:24:57 -0700

    Implemented create_tables function

---
+++ Weatherstation-API/Model/create_tables.py
@@ -0,0 +1,11 @@
+from sensor_reading_entity import SensorReading
+
+
+def create_tables() -> None:
+    """
+    This function is used to create the tables for each Entity in our Model.
+    Make sure the database connection to the database is already established
+    BEFORE calling this function
+    :return: None
+    """
+    SensorReading.createTable()
--- Weatherstation-API/server.py
+++ Weatherstation-API/server.py
@@ -2,14 +2,15 @@ from flask import Flask
 from Model.sensor_reading_entity import SensorReading
 from sqlobject import connectionForURI, sqlhub
 import constants
-from Controller.sensor_readings_controller import
+from Controller.sensor_readings_controller import add_sensor_reading
+from Model.create_tables import create_tables
 
 # Create a connection to the SQLite database
 sqlite_connection = connectionForURI(constants.DATABASE_PATH)
 sqlhub.processConnection = sqlite_connection
 
-# Create the SensorReading table
-SensorReading.createTable()
+# Create the tables in the database
+create_tables()
 
 # Create the Flask instance, this will be used to create the api endpoints
 app = Flask(__name__)
