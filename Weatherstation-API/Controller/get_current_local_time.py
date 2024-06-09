46a9e84c343717d5b70a10747052df90eb5bfcd1
Brendan Caldwell <brendanshauncaldwell@gmail.com>
2024-06-09 16:13:51 -0700

    Made the get_current_local_time controller retrieve the time from the clock on the device rather than World Time API

--- Weatherstation-API/Controller/get_current_local_time.py
+++ Weatherstation-API/Controller/get_current_local_time.py
@@ -5,9 +5,10 @@ import constants
 
 
 def get_current_local_time() -> datetime.datetime:
-    timezone = dotenv.get_key(constants.DOTENV_FILE_PATH, "TIME_ZONE")
-
-    res = requests.get(f'http://worldtimeapi.org/api/timezone/{timezone}').json()
-    print(res)
-
-    return datetime.datetime.fromisoformat(res.get('datetime'))
+    # timezone = dotenv.get_key(constants.DOTENV_FILE_PATH, "TIME_ZONE")
+    #
+    # res = requests.get(f'http://worldtimeapi.org/api/timezone/{timezone}').json()
+    # print(res)
+    #
+    # return datetime.datetime.fromisoformat(res.get('datetime'))
+    return datetime.datetime.now()
