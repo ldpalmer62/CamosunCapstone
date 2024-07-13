fd3329d398cbbf1a647efdc85e3ef2482df42e6c
JunoDrago <calebolsen34@gmail.com>
2024-07-13 16:11:41 -0700

    Added comments
    I know i should've done this as i went but owell

--- Weather Data Recorder-Wireframe/scripts/previous-pressures.js
+++ Weather Data Recorder-Wireframe/scripts/previous-pressures.js
@@ -1,7 +1,11 @@
-var ProcessedData;
+var ProcessedData;//declared here to keep public
+const BASE_URL = "http://205.250.221.237:8080";
+
+//Fetches all data of a sensor from the database
+//Params: sensorID
+//Returns: array of data point objects
 const FetchData = async sensorID =>{
   try {
-    const BASE_URL = "http://205.250.221.237:8080";
     const response = await fetch(`${BASE_URL}/get_sensor_data_list?id=${sensorID}&&time_period=all`);
     const data = await response.json();
     return data.sensor_data;
@@ -11,12 +15,17 @@ const FetchData = async sensorID =>{
   }
 }
 
+//Compile all the fetched data into low/avg/high datapoints for each day
+//Params: datapoints from database
+//Returns: object containing each array
 const ProcessData = data => {
+  //Declare object with empty arrays
   let ProcessedData = {
     dataLow: [],
     dataAvg: [],
     dataHigh:[]
   }
+  //Declare needed variables
   var low;
   var avg;
   var numPoints;
@@ -24,13 +33,16 @@ const ProcessData = data => {
   var previous = data[0];
 
   data.forEach(dataPoint => {
+    //If the day of this datapoint is not the same as the previous datapoint
+    //  Put the previous days data into the associated arrays in the object
     if((new Date(dataPoint.date)).getDate() != (new Date(previous.date)).getDate()){
-      let day = new Date(previous.date);
-      day.setHours(0, 0, 0, 0);
+      let day = new Date(previous.date);//get the date of the previous day
+      day.setHours(0, 0, 0, 0);//To make sure the datapoints align with the graph
       ProcessedData.dataLow.push({x: day, y: low});
       ProcessedData.dataAvg.push({x: day, y: avg/numPoints});
       ProcessedData.dataHigh.push({x: day, y: high});
 
+      //Reset the variables to prepare for the next datapoints
       low = null;
       avg = 0;
       numPoints = 0;
@@ -47,19 +59,23 @@ const ProcessData = data => {
     numPoints++;
     previous = dataPoint;
   });
+  //Put the last days datapoints into arrays
   let day = new Date(previous.date);
   ProcessedData.dataLow.push({x: day, y: low});
   ProcessedData.dataAvg.push({x: day, y: avg/numPoints});
   ProcessedData.dataHigh.push({x: day, y: high});
+
   return ProcessedData;
 }
 
-
+//Loads the graph for the user
+//Params: Month: String, Year: int, data: processedData
 const LoadGraph = async (month, year, data) => {
   var dataLow = [];
   var dataHigh = [];
   var dataAvg = [];
 
+  //Get all the datapoints for the month/year the user specified
   for(let i = 0; i < data.dataLow.length; i++){
     if((new Date(data.dataLow[i].x)).getUTCMonth() == MonthToNumber(month) - 1 && (new Date(data.dataLow[i].x)).getUTCFullYear() == year){
       dataLow.push(data.dataLow[i]);
@@ -69,7 +85,7 @@ const LoadGraph = async (month, year, data) => {
   }
 
 
-
+  //Color theme
   CanvasJS.addColorSet(
     "Pressure",
     [
@@ -80,6 +96,7 @@ const LoadGraph = async (month, year, data) => {
       "#90EE90"                
     ]
   );
+
   var chart = new CanvasJS.Chart("chartContainer",
   {
     backgroundColor: "#ddddFF",
@@ -120,8 +137,9 @@ const LoadGraph = async (month, year, data) => {
 }
 
 
-///Converts a string of a month to the number of a month
-///EX: MonthToNumber("July") returns 7
+//Converts a string of a month to the number of a month
+//Params: month: String
+//Returns: int
 const MonthToNumber = month => {
   switch (month){
     case "January":
